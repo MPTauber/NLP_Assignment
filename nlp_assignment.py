b9024655b8fe52b7cfbb4da0d7a18b6ef3bf50f4
Madeleine Tauber <Madeleine_Tauber1@baylor.edu>
2020-03-06 13:21:33 -0600

    Create

--- nlp_assignment.py
+++ nlp_assignment.py
@@ -2,8 +2,20 @@
 ## nlp_venv/scripts/activate
 ## pip install textblob
 ## py -m textblob.download_corpora
+# pip install wordcloud
+# pip install imageio
+
+
 from textblob import TextBlob
 import nltk
 #nltk.download("stopwords") # only have to do this once
 from nltk.corpus import stopwords
 from pathlib import Path
+
+from wordcloud import WordCloud
+import imageio
+
+text = Path("book of John text.txt").read_text()
+
+mask_image = imageio.imread("mask_oval.png")
+
