389e8a20e2188acab65fe84ae56387bdd67cbb24
Madeleine Tauber <Madeleine_Tauber1@baylor.edu>
2020-03-06 17:38:57 -0600

    trying with string

--- nlp_assignment.py
+++ nlp_assignment.py
@@ -20,14 +20,22 @@ blob = TextBlob(Path("book of John text.txt").read_text())
 items = blob.word_counts.items()
 items_not_in_stops = [item for item in items if item[0] not in stops]
 
-
 #######
 # Get the 15 most common words
 from operator import itemgetter
 
 sorted_items = sorted(items_not_in_stops, key = itemgetter(1), reverse = True)
 
-top15 = sorted_items[:16]
+top15_items = sorted_items[:16]
+
+
+
+top15_list = [(x[0] + " ") * x[1] for x in top15_items]
+
+### Make list into string for wordcloud
+top15_str = "" 
+for x in top15_list:  
+    top15_str += x
 
 #######
 # Now make word cloud
@@ -36,8 +44,8 @@ import imageio
 
 import matplotlib as plt
 
-wordcloud = wordcloud.generate(top15)
 mask_image = imageio.imread("mask_oval.png")
-wordcloud = WordCloud(colormap = "PuRd", masm=mask_image, background_color="white")
+wordcloud = WordCloud(colormap = "PuRd", mask=mask_image, background_color="white")
+wordcloud = wordcloud.generate(top15_str)
+
 wordcloud = wordcloud.to_file("BookOfJohnWordCloud.png")
-plt.imshow(wordcloud)
