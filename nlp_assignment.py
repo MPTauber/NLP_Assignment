3e5218df365dbf54c4f54b2d9b5a58a645944e8f
Madeleine Tauber <Madeleine_Tauber1@baylor.edu>
2020-03-06 16:56:34 -0600

    convert text to blob, and get stop words from blob

--- nlp_assignment.py
+++ nlp_assignment.py
@@ -12,10 +12,20 @@ import nltk
 from nltk.corpus import stopwords
 from pathlib import Path
 
-from wordcloud import WordCloud
+#from wordcloud import WordCloud
 import imageio
 
+stops = stopwords.words("english")
+
 text = Path("book of John text.txt").read_text()
+blob = TextBlob(text)
 
+print([word for word in blob.words if word not in stops])
+
+
+"""
+wordcloud = wordcloud.generate(_________)
 mask_image = imageio.imread("mask_oval.png")
-
+wordcloud = WordCloud(colormap = "PuRd", masm=mask_image, background_color="white")
+wordcloud = wordcloud.to_file("BookOfJohnWordCloud.png")
+plt.imshow(wordcloud)"""
